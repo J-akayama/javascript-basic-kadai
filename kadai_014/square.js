369e176b05becaeb3c5c14b8b4fc8e7aff220b36
J-akayama <ta.200206@gmail.com>
2023-10-01 17:34:05 +0900

    課題提出

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,7 @@
 // 引数で渡した値を二乗した計算結果を戻り値として返す関数
 const calculate = (total) => {
-  console.log(total * total);
+  return total * total;
 }
 
 // 関数を呼び出し、引数として金額を渡す
-calculate(10);
+console.log(calculate(10));
