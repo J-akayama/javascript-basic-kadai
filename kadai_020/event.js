2a6d5e20768c59a4dc3a7435075ec11dd0e7c187
J-akayama <ta.200206@gmail.com>
2023-10-06 20:00:31 +0900

    commit

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,6 +3,6 @@ const outputBtn  = document.getElementById('btn');
 // クリックされたときにイベント処理を実行する
 outputBtn .addEventListener('click' ,() => {
 //'text'を書き換える
-    const textChange = document.getElementById('text');
+const textChange = document.getElementById('text');
     textChange.textContent = 'ボタンをクリックしました'
 });
